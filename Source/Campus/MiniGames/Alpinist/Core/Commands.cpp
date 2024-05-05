b32c7a9977b09f099195bfa67d335eb1122d9a1f
Mikhail <47919432+MEgorov4@users.noreply.github.com>
2024-05-06 04:17:31 +0500

    Create Commands.cpp

---
+++ Source/Campus/MiniGames/Alpinist/Core/Commands.cpp
@@ -0,0 +1,71 @@
+#include "Commands.h"
+#include "GameController.h"
+
+using namespace AlpinistGame;
+
+bool RotateRightCommand::Execute()
+{
+	if (!m_gameController)
+		return false;
+	return m_gameController->RotateRight();
+}
+
+bool RotateLeftCommand::Execute()
+{
+	if (!m_gameController)
+		return false;
+	return m_gameController->RotateLeft();
+}
+
+bool MoveCommand::Execute()
+{
+	if (!m_gameController)
+		return false;
+	return m_gameController->MoveForward();
+}
+
+bool MacroCommand::Execute()
+{
+	for (PlayerCommand* playerCommand : m_commandList)
+	{
+		if (!playerCommand->Execute())
+			return false;
+	}
+	return true;
+}
+
+void MacroCommand::PushCommand(PlayerCommand* playerCommand)
+{
+	m_commandList.push_back(playerCommand);
+}
+
+void ConditionCommand::ToggleResult()
+{ m_shouldBeNegation = !m_shouldBeNegation; }
+
+bool ConditionCommand::Execute()
+{
+	if (!m_gameController)
+		return false;
+
+	m_bResult = m_gameController->WallInDirection(m_definition);
+	return true;
+}
+
+bool IfCommand::Execute()
+{
+	if (m_bConditon)
+	{
+		for (PlayerCommand* playerCommand : m_commandListIfTrue)
+		{
+			if (!playerCommand->Execute())
+				return false;
+		}
+		return true;
+	}
+	for (PlayerCommand* playerCommand : m_commandListIfFalse)
+	{
+		if (!playerCommand->Execute())
+			return false;
+	}
+	return true;
+}
